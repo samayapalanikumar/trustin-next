3e09c80f66d34429f788a6e87572e5bed727efa4
githiyon <githiyonkowshik@gmail.com>
2024-03-22 11:26:06 +0530

    add border

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -164,7 +164,7 @@ export default function SignIn() {
             </div>
           </div>
 
-          <div className="w-full border-stroke dark:border-strokedark lg:w-1/2 xl:border-l-2">
+          <div className="w-full border-stroke dark:border-strokedark lg:w-1/2 lg:border-l-2">
             <div className="w-full p-4 sm:p-12.5 xl:p-17.5">
               <h2 className="mb-9 text-2xl font-bold text-black dark:text-white sm:text-title-xl2">
                 Sign In to Trustin

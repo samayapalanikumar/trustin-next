5392747dccf6a87b57bf06b223908a7b617c2c89
Karthi A <karthikthee7@gmail.com>
2024-04-01 18:55:55 +0530

    fix logi not found

--- app/trf/success/page.tsx
+++ app/trf/success/page.tsx
@@ -1,6 +1,6 @@
 import ECommerce from "@/components/Dashboard/E-commerce";
 import { Metadata } from "next";
-import image from "@/app/image/logi.png"
+// import image from "@/app/image/logi.png"
 import Image from "next/image";
 
 export const metadata: Metadata = {
@@ -15,7 +15,7 @@ export default function DashboardPage() {
     <div >
   <div className="flex justify-center items-center  mt-50  md:flex-1 sm:flex">
     <div className="p-15 flex flex-col justify-center items-center shadow-xl bg-zinc-50 rounded   ">
-    <Image className="p-2" src={image} width={80} height={80} alt=" "/>
+    {/* <Image className="p-2" src={image} width={80} height={80} alt=" "/> */}
       <p className="font-black italic text-2xl text-left md:text-center">Test Request Form Submitted Successfully..!</p>
     </div>
   </div>

ea7adf698b472f7c45efd710ea9ec14e1065d48d
Karthi A <karthikthee7@gmail.com>
2024-04-01 19:42:39 +0530

    fix type issue

--- app/(auth)/signin/signin-form.tsx
+++ app/(auth)/signin/signin-form.tsx
@@ -55,6 +55,7 @@ const SigninForm = () => {
             type="email"
             name="username"
             placeholder="Enter your email"
+            autoComplete="email"
             className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
           />
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,9 @@
-'use client'
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { redirect } from "next/navigation";
 
 
 
 export default function Home() {
-  const router = useRouter();
 
-  useEffect(() => {
-    // Redirect to the '/dashboard' route when the component mounts
-    router.push('/dashboard');
-  }, []);
-
-  // This component doesn't need to render anything as it redirects immediately
-  return null;
+  redirect('/dashboard')
 
 }

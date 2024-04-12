03d365f32c0984bbb827346c018f99868cb031ff
samaya palani kumar <samayahari143@gmail.com>
2024-04-12 15:02:39 +0530

    add image and text

--- app/trf/success/page.tsx
+++ app/trf/success/page.tsx
@@ -14,8 +14,8 @@ export default function DashboardPage() {
     <> 
     <div >
   <div className="flex justify-center items-center  mt-50  md:flex-1 sm:flex">
-    <div className="p-15 flex flex-col justify-center items-center shadow-xl bg-zinc-50 rounded   ">
-    {/* <Image className="p-2" src={image} width={80} height={80} alt=" "/> */}
+    <div className="p-15 flex flex-col justify-center items-center shadow-xl bg-gray-100">
+    <Image className="p-2" src={"/images/trf/img3.webp"} width={150} height={130} alt="success"/>
       <p className="font-black italic text-2xl text-left md:text-center">Test Request Form Submitted Successfully..!</p>
     </div>
   </div>

8cb4e4f07dfeb60d6eba857faa91c9f512651c54
githiyon <githiyonkowshik@gmail.com>
2024-04-15 11:57:05 +0530

    change image

--- app/dashboard/error.tsx
+++ app/dashboard/error.tsx
@@ -17,7 +17,7 @@ export default function Error({
  
   return (
     <div className='flex flex-col items-center justify-center'>
-        <div className='lg:h-60'>
+        <div className='h-52 sm:h-72 lg:h-96'>
       <Image
            className="w-full h-full"
            src={"/images/error/error-img-two.webp"}
@@ -28,11 +28,11 @@ export default function Error({
         />
         </div>
         <div className='grid items-center justify-items-center'>
-      <h2 className='text-2xl lg:text-3xl text-center font-bold'>Something went wrong!</h2>
+      <h2 className='text-2xl lg:text-3xl mb-3 text-center font-bold'>Something went wrong!</h2>
      
       <button
       //  className='bg-indigo-500 py-2 px-4 text-xl text-center my-3 text-white rounded-lg'
-      className='rounded-md bg-primary px-3.5 py-2.5 my-3 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary'
+      className='rounded-md bg-primary px-3.5 py-2.5 lg:px-4 lg:py-3 my-3 text-sm lg:text-base font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary'
         onClick={
           // Attempt to recover by trying to re-render the segment
           () => reset()
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,7 +5,7 @@ import { MoveLeft } from 'lucide-react';
 export default function NotFound() {
   return <>
       <div className='min-h-screen  flex flex-col items-center justify-center w-11/12 mx-auto text-center'>
-        <div className='my-0 xl:h-60 lg:h-40 '>
+        <div className='my-0 h-36 lg:h-40 xl:h-60 '>
       <Image
            className="w-full h-full"
            src={"/images/error/error-img.png"}
@@ -19,14 +19,14 @@ export default function NotFound() {
       <h2 className='my-0 text-2xl lg:text-3xl text-center font-bold'>Not Found</h2>
       <p className="text-xl font-bold">Could not find requested resource</p>
      
-      <button
+       <Link href="/dashboard"
       //  className='bg-indigo-500 py-2 px-4 text-xl text-center my-3 text-white rounded-lg'
-      className='rounded-md bg-primary px-3.5 py-2.5 my-3 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary'
+      className='flex items-center justify-center gap-2 rounded-md px-2.5 py-3 text-sm bg-primary lg:px-4 lg:py-5 my-3 lg:text-base font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary'
       >
         <MoveLeft />
-         <Link href="/dashboard">Return Dashboard</Link>
+        Return Dashboard</Link>
         
-      </button>
+    
       </div>
       </div>
       </>

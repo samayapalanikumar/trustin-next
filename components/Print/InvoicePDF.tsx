f1667c456ad66e7051f9660ff6cb9c5015d5e2db
Karthi A <karthikthee7@gmail.com>
2024-04-23 21:27:09 +0530

    added test report pdf

--- components/Print/InvoicePDF.tsx
+++ components/Print/InvoicePDF.tsx
@@ -164,6 +164,7 @@ const MyDocument = ({ data }: { data: any }) => {
             <View style={{ marginTop: 5, border: "1 solid #000" }}>
               <Text>Customer Information</Text>
             </View>
+
             <View
               style={{
                 border: "1 solid #000",
@@ -190,9 +191,16 @@ const MyDocument = ({ data }: { data: any }) => {
               }}
             >
               <Text style={{ borderRight: "1 solid #000", width: 180 }}>
-                Batch No
+                Address
               </Text>
-              <Text>{data.sample.batch.batch_no}</Text>
+              <Text>
+                {data.sample.registration.customer_address_line1 +
+                  " " +
+                  data.sample.registration.customer_address_line2}
+              </Text>
+            </View>
+            <View style={{ marginTop: 5, border: "1 solid #000" }}>
+              <Text>Sample Information</Text>
             </View>
             <View
               style={{
@@ -205,7 +213,56 @@ const MyDocument = ({ data }: { data: any }) => {
               }}
             >
               <Text style={{ borderRight: "1 solid #000", width: 180 }}>
-                Test Type
+                Batch No
+              </Text>
+              <Text style={{ borderRight: "1 solid #000", width: 180 }}>
+                {data.sample.batch.batch_no}
+              </Text>
+
+              <Text style={{ borderRight: "1 solid #000", width: 100 }}>
+                Batch Size
+              </Text>
+              <Text>{data.sample.batch.batch_size}</Text>
+            </View>
+            <View
+              style={{
+                border: "1 solid #000",
+                display: "flex",
+                flexDirection: "row",
+                fontWeight: "bold",
+                fontSize: "12px",
+                padding: 1,
+              }}
+            >
+              <Text style={{ borderRight: "1 solid #000", width: 180 }}>
+                Mfg No
+              </Text>
+              <Text style={{ borderRight: "1 solid #000", width: 180 }}>
+                {new Date(
+                  data.sample.batch.manufactured_date,
+                ).toLocaleDateString()}
+              </Text>
+
+              <Text style={{ borderRight: "1 solid #000", width: 100 }}>
+                Exp Size
+              </Text>
+              <Text>
+                {" "}
+                {new Date(data.sample.batch.expiry_date).toLocaleDateString()}
+              </Text>
+            </View>
+            <View
+              style={{
+                border: "1 solid #000",
+                display: "flex",
+                flexDirection: "row",
+                fontWeight: "bold",
+                fontSize: "12px",
+                padding: 1,
+              }}
+            >
+              <Text style={{ borderRight: "1 solid #000", width: 180 }}>
+                Test Method
               </Text>
               <Text>{data.sample.test_type_id === 2 ? "Mech" : "Micro"}</Text>
             </View>
@@ -241,6 +298,25 @@ const MyDocument = ({ data }: { data: any }) => {
               </Text>
               <Text>
                 {data?.sample?.registration?.product_data?.product_code}
+              </Text>
+            </View>
+            <View
+              style={{
+                border: "1 solid #000",
+                display: "flex",
+                flexDirection: "row",
+                fontWeight: "bold",
+                fontSize: "12px",
+                padding: 1,
+              }}
+            >
+              <Text style={{ borderRight: "1 solid #000", width: 180 }}>
+                Date of Received
+              </Text>
+              <Text>
+                {new Date(
+                  data?.sample?.registration?.date_of_received,
+                ).toLocaleDateString()}
               </Text>
             </View>
           </View>
@@ -294,6 +370,51 @@ const MyDocument = ({ data }: { data: any }) => {
                 <Text>{item.result ? "Pass" : "Fail"}</Text>
               </View>
             ))}
+          </View>
+          <View
+            style={{
+              display: "flex",
+              fontSize: 12,
+            }}
+          >
+            <Text style={{ fontWeight: "bold", textAlign: "left" }}>
+              Opinion and intrepretation (if any):
+            </Text>
+            <Text style={{ marginLeft: 20, textAlign: "left", fontSize: 10 }}>
+              Requistion made by the customer with repect to the above test
+              only.
+            </Text>
+          </View>
+
+          <View
+            style={{
+              marginTop: 100,
+              display: "flex",
+              justifyContent: "flex-end",
+              flexDirection: "column",
+              fontSize: 14,
+            }}
+          >
+            <Text style={{ fontWeight: "bold", textAlign: "right" }}>
+              Authorized Signatory{" "}
+            </Text>
+          </View>
+          <View
+            style={{
+              marginTop: 150,
+              display: "flex",
+              flexDirection: "column",
+              fontSize: 10,
+            }}
+          >
+            <Text style={{ fontWeight: "bold", textAlign: "center" }}>
+              * This test report shall not be reproduced except in full, without
+              written approval of the laboratory. *
+            </Text>
+            <Text style={{ fontWeight: "bold", textAlign: "center" }}>
+              * The test results in this report refer only to the sample tested
+              in the laboratory and the sample submitted by the party. *
+            </Text>
           </View>
         </View>
       </Page>

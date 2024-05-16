20d4cb392eda450fdcbf8d945d7a946c275f2bb6
githiyon <githiyonkowshik@gmail.com>
2024-05-16 16:35:45 +0530

    add batches folder

---
+++ app/dashboard/batches/page.tsx
@@ -0,0 +1,68 @@
+import TableThree from "@/components/Tables/TableThree";
+import Link from "next/link";
+
+
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
+import BatchesTable,{BatchesType} from "./batches-table";
+import { SERVER_API_URL } from "@/app/constant";
+
+import { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+  title: "Batches | Trustin",
+  description: "This is Batches page ",
+  // other metadata
+};
+
+async function getData() {
+  const cookieStore = cookies();
+  const access_token = cookieStore.get("access_token");
+    const res = await fetch(`${SERVER_API_URL}/Batches/`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${access_token?.value}`,
+      },
+      next:{
+        tags:['Batches'],
+      }
+    });
+    // The return value is *not* serialized
+    // You can return Date, Map, Set, etc.
+
+    if (!res.ok) {
+      // This will activate the closest `error.js` Error Boundary
+      // console.log(res)
+      // throw new Error("Failed to fetch data");
+      console.log("error");
+    }
+    if(res.status===401) redirect('/signin');
+    const resjson = await res.json();
+    return resjson;
+ 
+}
+
+const BatchesPage = async () => {
+  const data:BatchesType = await getData()
+  return (
+    <>
+      <div className="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
+        <h2 className="text-title-md2 font-semibold text-black dark:text-white">
+        Batches
+        </h2>
+        <Link
+          href="Batches/new"
+          className="inline-flex items-center justify-center rounded-md border border-black py-4 px-5 text-center font-medium text-black hover:bg-opacity-90 lg:px-8 xl:px-10 dark:text-white dark:border-white"
+        >
+          New Batches
+        </Link>
+      </div>
+      <div className="flex flex-col gap-10">
+        <BatchesTable data={data} />
+      </div>
+    </>
+  );
+};
+
+export default BatchesPage;
---
+++ app/dashboard/samples/new/samples-add-form.tsx
@@ -0,0 +1,310 @@
+"use client";
+import { useEffect, useState } from "react";
+import { useFieldArray, useForm, useWatch, Form } from "react-hook-form";
+import { Trash2 } from "lucide-react";
+import Select from "@/components/select-input";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+
+type Sample = {
+  name: string;
+  batch_id: number;
+  test_type_id: string;
+  test_params: Array<{
+    test_parameter_id: string;
+    order: number;
+  }>;
+};
+
+type FormDatas = {
+  samples: Sample[];
+};
+
+type InitialState = {
+  fieldErrors?: {} | null;
+  type?: string | null;
+  message?: any | string | null;
+};
+
+const initialState: InitialState = {
+  fieldErrors: {},
+  type: null,
+  message: null,
+};
+
+const SamplesAddForm = ({
+  data
+}: {
+  data: any;
+}) => {
+  const {
+    control,
+    register,
+    formState: { isLoading, isSubmitting },
+    handleSubmit,
+  } = useForm<FormDatas>({
+    defaultValues: {
+     
+        name: "",
+        batch_id: "",
+        test_type_id: "1",
+        test_params: [
+          {
+            test_parameter_id: "",
+            order: "",
+          },
+        ],
+     
+    },
+  });
+  // const { fields, append, remove, replace } = useFieldArray({
+  //   control: control,
+  //   name: "samples", // Name of the array in your schema
+  // });
+
+  const sampleWatch = useWatch({
+    control,
+    name: "test_type_id",
+  });
+
+  const [filterId, setFilterId] = useState(
+    data.batches.map((batch: any, idx: number) => 1),
+  );
+
+  useEffect(() => {
+    // TODO: need some imporvement in future
+    // const ids = sampleWatch.map((field, idx) => field.test_type_id);
+    // setFilterId(ids);
+  }, [sampleWatch]);
+
+  const [state, setState] = useState<InitialState | undefined>(initialState);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (state?.type === null) return;
+
+    if (state?.type === "Error") {
+      toast.error(state?.message, {
+        duration: 10000,
+        closeButton: true,
+      });
+    }
+    if (state?.type === "Success") {
+      toast.success(state?.message, {
+        duration: 10000,
+        closeButton: true,
+      });
+      router.push("/dashboard/samples");
+    }
+  }, [state, router]);
+
+  const handleForm = async (data: FormDatas) => {
+    // const res = await createFn(data);
+    // setState(res);
+    console.log(data)
+  };
+
+  return (
+    <form onSubmit={handleSubmit(handleForm)}>
+      <div className="p-6.5">
+       
+          <div>
+           
+            <div className="mb-4.5 flex flex-col gap-6 xl:flex-row">
+              <div className="w-full xl:w-1/4">
+                <label className="mb-2.5 block text-black dark:text-white">
+                  Batch
+                </label>
+
+                <div className="relative z-20 bg-transparent dark:bg-form-input">
+                  <select
+                    {...register(`batch_id`)}
+                    className="relative z-20 w-full appearance-none rounded border border-stroke bg-transparent px-5 py-3 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+                  >
+                    <option value={0}>------------</option>
+                    {data?.batches?.map((t: any) => (
+                      <option value={t.id} key={t.id}>
+                        {t.batch_no}
+                      </option>
+                    ))}
+                  </select>
+                  <span className="absolute right-4 top-1/2 z-30 -translate-y-1/2">
+                    <svg
+                      className="fill-current"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <g opacity="0.8">
+                        <path
+                          fillRule="evenodd"
+                          clipRule="evenodd"
+                          d="M5.29289 8.29289C5.68342 7.90237 6.31658 7.90237 6.70711 8.29289L12 13.5858L17.2929 8.29289C17.6834 7.90237 18.3166 7.90237 18.7071 8.29289C19.0976 8.68342 19.0976 9.31658 18.7071 9.70711L12.7071 15.7071C12.3166 16.0976 11.6834 16.0976 11.2929 15.7071L5.29289 9.70711C4.90237 9.31658 4.90237 8.68342 5.29289 8.29289Z"
+                          fill=""
+                        ></path>
+                      </g>
+                    </svg>
+                  </span>
+                </div>
+              </div>
+
+              <div className="w-full xl:w-1/4">
+                <label className="mb-2.5 block text-black dark:text-white">
+                  Sample Name
+                </label>
+                <input
+                  type="text"
+                  {...register(`name`)}
+                  placeholder="Enter Sample Name"
+                  className="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+                />
+              </div>
+
+              {/* <div className="w-full xl:w-1/4">
+                <label className="mb-2.5 block text-black dark:text-white">
+                  Department
+                </label>
+                <input
+                  type="text"
+                  {...register(`samples.${index}.department`)}
+                  placeholder="Enter Test Type"
+                  className="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+                />
+              </div>*/}
+
+              <div className="w-full xl:w-1/4">
+                <label className="mb-2.5 block text-black dark:text-white">
+                  Batch
+                </label>
+
+                <div className="relative z-20 bg-transparent dark:bg-form-input">
+                  <select
+                    {...register(`test_type_id`)}
+                    className="relative z-20 w-full appearance-none rounded border border-stroke bg-transparent px-5 py-3 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+                  >
+                    <option value={"1"}>Micro</option>
+                    <option value={"2"}>Mech</option>
+                  </select>
+                  <span className="absolute right-4 top-1/2 z-30 -translate-y-1/2">
+                    <svg
+                      className="fill-current"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <g opacity="0.8">
+                        <path
+                          fillRule="evenodd"
+                          clipRule="evenodd"
+                          d="M5.29289 8.29289C5.68342 7.90237 6.31658 7.90237 6.70711 8.29289L12 13.5858L17.2929 8.29289C17.6834 7.90237 18.3166 7.90237 18.7071 8.29289C19.0976 8.68342 19.0976 9.31658 18.7071 9.70711L12.7071 15.7071C12.3166 16.0976 11.6834 16.0976 11.2929 15.7071L5.29289 9.70711C4.90237 9.31658 4.90237 8.68342 5.29289 8.29289Z"
+                          fill=""
+                        ></path>
+                      </g>
+                    </svg>
+                  </span>
+                </div>
+              </div>
+            </div>
+            {/* // Test Params */}
+            <TestParamsForm
+              filterId={filterId}
+              data={data}
+              {...{ control, register }}
+            />
+          </div>
+    
+        <button
+          type="submit"
+          className="flex w-full justify-center rounded bg-primary p-3 font-medium text-gray"
+          disabled={isSubmitting}
+        >
+          {isSubmitting ? "Loading" : "Submit"}
+        </button>
+      </div>
+    </form>
+  );
+};
+
+const TestParamsForm = ({
+  control,
+  register,
+  data,
+  filterId,
+}: {
+  control: any;
+  register: any;
+  data: any;
+  filterId: [];
+}) => {
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: `test_params`,
+  });
+  return (
+    <div className="mb-4">
+      {fields.map((item, index) => (
+        <div key={item.id} className="mb-4 mt-2">
+          <div className="mb-2 flex  justify-between border-b-2">
+            <p>
+              Test Parameter <strong>#{index + 1}:</strong>
+            </p>
+            <div>
+              <button
+                type="button"
+                className="flex  justify-center rounded-full p-2   font-medium text-black hover:bg-gray "
+                onClick={() => remove(index)}
+              >
+                <Trash2 className="w-4" />
+              </button>
+            </div>
+          </div>
+          <div className="mb-4.5 flex flex-col gap-6 xl:flex-row">
+            <Select
+              name={`test_params.${index}.test_parameter_id`}
+              register={register}
+              label={"Test Parameter Name"}
+              width="w-full xl:w-1/5 "
+            >
+              <option value="">------------</option>
+              {data.test_params
+                ?.filter(
+                  (t: any) =>
+                    t.test_parameter.test_type_id.toString() ===
+                    filterId
+                )
+                .map((t: any) => (
+                  <option value={t.test_parameter.id} key={t.id}>
+                    {t.test_parameter.testing_parameters}
+                  </option>
+                ))}
+            </Select>
+            <div className="w-full xl:w-1/5">
+              <label className="mb-2.5 block text-black dark:text-white">
+                order
+              </label>
+              <input
+                type="number"
+                {...register(`test_params.${index}.order`)}
+                className="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+              />
+            </div>
+          </div>
+        </div>
+      ))}
+
+      <button
+        type="button"
+        className="mb-4 mt-2 flex justify-center rounded bg-primary p-3 font-medium text-gray"
+        onClick={() => append({ test_parameter_id: "", order: 0 })}
+      >
+        Add Test parameter
+      </button>
+    </div>
+  );
+};
+
+export default SamplesAddForm;
